5a3d1477a2a17a6298cd6c03a98ea9821bf339ac
Ben Gould <ben+dev@amb1ent.org>
2022-04-07 09:28:02 +0200

    point scripts to live

--- err0-insert.sh
+++ err0-insert.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-docker pull err0io/agent:develop && \
-docker run --network="host" --mount type=bind,source=`pwd`,destination=/mnt err0io/agent:develop \
+docker pull err0io/agent:latest && \
+docker run --mount type=bind,source=`pwd`,destination=/mnt err0io/agent:latest \
   /usr/local/bin/err0.sh --token /mnt/tokens/err0-err0-io-err0agent-dc4ccb4c-5da5-11ec-a0b8-4622287bbd85.json --insert /mnt
