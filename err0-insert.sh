20c2d2f54b61708b007c3fa2b66d32063359b68a
Ben Gould <ben@amb1ent.org>
2022-07-12 10:14:14 +0100

    .

--- err0-insert.sh
+++ err0-insert.sh
@@ -2,4 +2,4 @@
 
 docker pull err0io/agent:latest && \
 docker run --mount type=bind,source=`pwd`,destination=/mnt err0io/agent:latest \
-  /usr/local/bin/err0.sh --token /mnt/tokens/err0-err0-io-err0agent-dc4ccb4c-5da5-11ec-a0b8-4622287bbd85.json --insert /mnt
+  /usr/local/bin/err0.sh --token /mnt/tokens/err0agent.json --insert /mnt
