95cbe7d4fcac4b2d2a05a0ed750679f94e5261e6
Ben Gould <ben@amb1ent.org>
2023-07-25 16:06:25 +0100

    Fix issue with check pass.

---
+++ src/test/testdata/0010/02/a.py
@@ -0,0 +1,27 @@
+class Apps:
+    """
+    A registry that stores the configuration of installed applications.
+
+    It also keeps track of models, e.g. to provide reverse relations.
+    """
+
+    def __init__(self, installed_apps=()):
+        # installed_apps is set to None when creating the master registry
+        # because it cannot be populated at that point. Other registries must
+        # provide a list of installed apps and are populated immediately.
+        if installed_apps is None and hasattr(sys.modules[__name__], 'apps'):
+            raise RuntimeError("[E-1] You must supply an installed_apps argument.")
+
+        # Mapping of app labels => model names => model classes. Every time a
+        # model is imported, ModelBase.__new__ calls apps.register_model which
+
+class Example:
+
+    def method(list=()):
+        raise RuntimeError(", ".join("%s" % x for x in list)
+
+    def method2():
+        raise RuntimeError('''[E-2] This is an error''')
+
+    def method3():
+        raise RuntimeError("""[E-3] This is an error""")
