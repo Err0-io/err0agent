59cc1f5d3e23638453a173c8bc4282a1999e5645
Ben Gould <ben@amb1ent.org>
2023-07-28 12:49:12 +0100

    Add pytorch to the projects.

--- build.gradle.kts
+++ build.gradle.kts
@@ -143,6 +143,7 @@ tasks.withType<JavaExec> {
     "--token", "../open-source-bundle/dev-localhost/err0-cerbos-20221207-33d4bf05-763f-11ed-8b95-4401bb8de3b3.json", "--insert", "../open-source-bundle/cerbos", // go
     "--token", "../open-source-bundle/dev-localhost/err0-mender-20221207-5b47787b-763f-11ed-8b95-4401bb8de3b3.json", "--insert", "../open-source-bundle/mender", // go
     "--token", "../open-source-bundle/dev-localhost/err0-rails-20230723-82c92bf1-1997-11ee-8025-305a3ac84b71.json", "--insert", "../open-source-bundle/rails", // Ruby
+    "--token", "../open-source-bundle/dev-localhost/err0-pytorch-20221209-6cd848c3-77ab-11ed-b3c0-4401bb8de3b3.json", "--insert", "../open-source-bundle/pytorch", // Python/C++
     // pass #2 -- analyse source code, ignoring dirty checkout.
     "--token", "../open-source-bundle/dev-localhost/err0-leptos-20221220-7c62eafd-806b-11ed-b59e-4401bb8de3b3.json", "--analyse", "--dirty", "../open-source-bundle/leptos", // Rust
     "--token", "../open-source-bundle/dev-localhost/err0-postfix-20221220-5cd5d49b-806b-11ed-b59e-4401bb8de3b3.json", "--analyse", "--dirty", "../open-source-bundle/postfix", // C
@@ -164,6 +165,7 @@ tasks.withType<JavaExec> {
     "--token", "../open-source-bundle/dev-localhost/err0-cerbos-20221207-33d4bf05-763f-11ed-8b95-4401bb8de3b3.json", "--analyse", "--dirty", "../open-source-bundle/cerbos", // go
     "--token", "../open-source-bundle/dev-localhost/err0-mender-20221207-5b47787b-763f-11ed-8b95-4401bb8de3b3.json", "--analyse", "--dirty", "../open-source-bundle/mender", // go
     "--token", "../open-source-bundle/dev-localhost/err0-rails-20230723-82c92bf1-1997-11ee-8025-305a3ac84b71.json", "--analyse", "--dirty", "../open-source-bundle/rails", // Ruby
+    "--token", "../open-source-bundle/dev-localhost/err0-pytorch-20221209-6cd848c3-77ab-11ed-b3c0-4401bb8de3b3.json", "--analyse", "--dirty", "../open-source-bundle/pytorch", // Python/C++
   )
 
   //args = listOf("--version", "--help")
